241b513dc6289a33402548b76bb874b3ac75aded
Ayush Verma <ayushverma.8509@gmail.com>
2025-06-25 21:52:58 +0530

    scrrriipptsss

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
+gsap.from(".nlink", {
+  stagger: .2,
+  y: 10,
+  duration: 1.5,
+  ease: "power4.out",
+  opacity: 0
+});
+
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
+  el: document.querySelector('#main'),
+  smooth: true
+});
+
+
+Shery.textAnimate("#headings h1", {
+  //Parameters are optional.
+  style: 1,
+  duration: 2,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
 });
